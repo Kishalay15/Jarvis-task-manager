17f6e84634cb0aca9fb14fd51d9c2953a073f58f
Kishalay15 <kishalaylahiri@gmail.com>
2025-02-18 13:53:27 +0530

    frontend Start

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import Login from './pages/Login'
+import { Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom"
+import Dashboard from './pages/Dashboard'
+import Tasks from './pages/Tasks'
+import Users from './pages/Users'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Navigate to="/dashboard" />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/tasks' element={<Tasks />} />
+          <Route path='/completed/:status' element={<Tasks />} />
+          <Route path='/in-progress/:status' element={<Tasks />} />
+          <Route path='/todo/:status' element={<Tasks />} />
+          <Route path='/team' element={<Users />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/TaskContent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TaskContent() {
+    return (
+        <div>TaskContent</div>
+    )
+}
+
+export default TaskContent
