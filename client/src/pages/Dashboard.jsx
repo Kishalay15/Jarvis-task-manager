37282ade781df7d4925fc0fd54fc9cab582d7d08
Kishalay15 <kishalaylahiri@gmail.com>
2025-02-19 17:28:53 +0530

    dashboard done

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from "react";
+import {
+    ClipboardList,
+    ClipboardCheck,
+    ListTodo,
+    LayoutList
+} from "lucide-react";
+import { summary } from "../assets/data";
+import clsx from "clsx";
+import Chart from "../components/Chart";
 
-function Dashboard() {
+const Dashboard = () => {
+    const totals = summary.tasks;
+
+    const stats = [
+        {
+            _id: "1",
+            label: "TOTAL TASKS",
+            total: summary?.totalTasks || 0,
+            icon: <ClipboardList size={20} />,
+            bg: "bg-[#af7ac5]",
+        },
+        {
+            _id: "2",
+            label: "COMPLETED TASKS", // Fixed Typo
+            total: totals["completed"] || 0,
+            icon: <ClipboardCheck size={20} />,
+            bg: "bg-[#0f766e]",
+        },
+        {
+            _id: "3",
+            label: "TASKS IN PROGRESS",
+            total: totals["in progress"] || 0,
+            icon: <ListTodo size={20} />,
+            bg: "bg-[#f59e0b]",
+        },
+        {
+            _id: "4",
+            label: "TODOS",
+            total: totals["todo"] || 0, // Added fallback
+            icon: <LayoutList size={20} />,
+            bg: "bg-[#1d4ed8]",
+        },
+    ];
+
+    const Card = ({ label, count, bg, icon }) => {
+        return (
+            <div className='w-full h-32 bg-white p-5 shadow-md rounded-md flex items-center justify-between'>
+                <div className='h-full flex flex-1 flex-col justify-between'>
+                    <p className='text-base text-gray-600'>{label}</p>
+                    <span className='text-2xl font-semibold'>{count}</span>
+                    {/* <span className='text-sm text-gray-400'>{"110 last month"}</span> */}
+                </div>
+                <div className={clsx("w-10 h-10 rounded-full flex items-center justify-center text-white", bg)}>
+                    {icon}
+                </div>
+            </div>
+        );
+    };
+
     return (
-        <div>helooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</div>
-    )
-}
+        <div className='h-full py-4'>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-5'>
+                {stats.map(({ icon, bg, label, total }, index) => (
+                    <Card key={index} icon={icon} bg={bg} label={label} count={total} />
+                ))}
+            </div>
+            <div className='w-full bg-white my-16 p-4 rounded shadow-sm'>
+                <h4 className='text-xl text-gray-600 font-semibold'>Chart by Priority</h4>
+                <Chart />
+            </div>
+        </div>
+    );
+};
 
-export default Dashboard
+export default Dashboard;
